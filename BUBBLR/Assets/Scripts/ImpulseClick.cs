242f20ee94a694deb90084429dbe29adf1e2c5b1
Francesco Visco <francevisco@gmail.com>
2025-01-25 13:07:24 +0100

    [TestScene] ImpulseClick base script

--- BUBBLR/Assets/Scripts/ImpulseClick.cs
+++ BUBBLR/Assets/Scripts/ImpulseClick.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class ImpulseClick : MonoBehaviour{
 
+    private Vector3 target;
+
     public float ImpulseForce;
+    private Vector2 direction;
+    private float distance;
+    private float forceRate;
 
     void Start()
     {
+        target = transform.position;
     }
 
     void Update() 
     {
-        if (Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButton(0)) 
         {
-            Debug.Log("Click");
+            this.GetComponent<CircleCollider2D>().enabled = true;
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector3.MoveTowards(transform.position, target, 10000 * Time.deltaTime);
         }
         else
         {
+            this.GetComponent<CircleCollider2D>().enabled = false;
         }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Input.GetMouseButton(0) && other.gameObject.CompareTag("Bubble"))
+        {
+            	direction = transform.position - other.transform.position;
+				distance = direction.magnitude;
+				direction = direction.normalized;
+				forceRate = (ImpulseForce/ distance);
+				other.GetComponent<Rigidbody2D>().AddForce(-direction * (forceRate / other.GetComponent<Rigidbody2D>().mass));
+        }
+    }  
 }
+
+    
+    
--- BUBBLR/Assets/Scripts/Rebound.cs
+++ BUBBLR/Assets/Scripts/Rebound.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class Rebound : MonoBehaviour
 {
-    public bool Wall;
-    public bool Bubble;
-    
-    public float WallForce;
-    public float BubbleForce;
+  public bool Wall;
+  public bool Bubble;
+   
+  public float WallForce;
+  public float BubbleForce;
 
-    public Rigidbody2D rb;
-    public AudioSource WallSound;
-    public AudioSource BubbleSound;
+  public Rigidbody2D rb;
+  public AudioSource WallSound;
+  public AudioSource BubbleSound;
 
-    void Start()
-    {
+  void Start()
+  {
         
-    }
+  }
 
-    void Update()
+  void Update()
+  {
+
+  }
+
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    if(Wall == true)
     {
-
+      other.rigidbody.AddForce(-other.contacts[0].normal * WallForce, ForceMode2D.Impulse);
+      //WallSound.Play();
     }
-
-    void OnCollisionEnter2D(Collision2D other)
+      
+    if(Bubble == true)
     {
-      if(Wall == true)
-      {
-       other.rigidbody.AddForce(-other.contacts[0].normal * WallForce, ForceMode2D.Impulse);
-       //WallSound.Play();
-      }
-
-      if(Bubble == true)
-      {
-       other.rigidbody.AddForce(-other.contacts[0].normal * BubbleForce, ForceMode2D.Impulse);
-       //BubbleSound.Play();
-      }
-    }    
+      other.rigidbody.AddForce(-other.contacts[0].normal * BubbleForce, ForceMode2D.Impulse);
+      //BubbleSound.Play();
+    }
+  }    
 }
